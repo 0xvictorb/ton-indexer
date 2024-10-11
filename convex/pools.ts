0e85ec22cfc174582328c235f5c93dc63f55f835
0xvictorb <brandonobryan2701@gmail.com>
2024-10-12 02:37:37 +0700

    update actions

--- convex/pools.ts
+++ convex/pools.ts
@@ -1,4 +1,6 @@
-import { internalQuery } from "./_generated/server";
+import { internalQuery, mutation } from "./_generated/server";
+import { v } from "convex/values";
+import { ConvexError } from "convex/values";
 
 export const getPools = internalQuery({
   args: {},
@@ -8,5 +10,21 @@ export const getPools = internalQuery({
       .collect();
 
     return pools;
+  },
+});
+
+export const create = mutation({
+  args: {
+    address: v.string(),
+    name: v.string(),
+    secret: v.string(),
+    contractName: v.union(v.literal('stonfi'), v.literal('dedust'), v.literal('utyab')),
+  },
+  handler: async (ctx, args) => {
+    if (args.secret !== process.env.SECRET) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    await ctx.db.insert("pools", args);
   },
 });
